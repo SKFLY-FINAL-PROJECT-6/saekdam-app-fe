787d989968c1a2d21961b2dc5994ff696f746e9f
kang-bm <kbm20008@naver.com>
2025-02-14 09:16:42 +0900

    splasescreen add

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class MyApp extends StatelessWidget {
       ),
       // 이미 로그인 상태면 HomeScreen 바로 보여주고,
       // 아니면 WelcomeScreen 먼저 보여줌
-      home: isLoggedIn ? const HomeScreen() :  WelcomeScreen(),
+      home: isLoggedIn ? const HomeScreen() : const WelcomeScreen(),
       // home: HomeScreen(),
       // 필요 시 라우트 등록
       routes: {
